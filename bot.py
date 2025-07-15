5b68fc254054d4fb8b41908b0f05914df96387db
Alveeeep <alvir.misalimov@gmail.com>
2025-07-16 01:19:07 +0300

    Deploy

--- bot.py
+++ bot.py
@@ -2,13 +2,15 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from handlers import group_checker
 from config import config
+import logging
 
-
+logger = logging.getLogger(__name__)
 ALLOWED_UPDATES = ['message, edited_message']
 
 
 # Запуск бота
 async def main():
+    logger.info("Starting bot...")
     bot = Bot(token=config.TOKEN.get_secret_value())
     dp = Dispatcher()
     dp.include_router(group_checker.router)
